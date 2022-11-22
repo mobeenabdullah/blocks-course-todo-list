8cf6fb21b6805200377f8ca459392625d224366b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-22 20:15:41 +0500

    Using the API to toggle the Todos

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0ed5cfd4725427a82d3d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ba4492ccaf29af706b77');
