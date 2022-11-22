17642c7daa449d1237c5cd8bc848120fee9b07ec
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-22 20:09:45 +0500

    Using the API to add new Todos

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '72aa1677b781db66778f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0ed5cfd4725427a82d3d');
